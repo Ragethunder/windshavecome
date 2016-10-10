8aee6d8ea9b2b8db15278fc3d0963f65239c2f88
Vladimir Peric <Vladimir Peric>
2016-10-10 22:18:42 +0200

    added chat channels

--- app.js
+++ app.js
@@ -53,10 +53,10 @@ io.on('connection', function(socket) {
 		var newPlayer = new Player(idNum, socket);
 		players.push(newPlayer);
 		
-		socket.emit('playerData', {id: idNum, players: players});
+		/*socket.emit('playerData', {id: idNum, players: players});
 		socket.emit('chatMessages', {messages:messagesGeneral});
-		socket.emit('chatMessages', {messages:messagesDevChat});
-		/*socket.broadcast.emit('playerConnected', newPlayer);*/
+		socket.emit('chatMessages', {messages:messagesDevChat});*/
+		socket.broadcast.emit('playerConnected', newPlayer);
 	});
 	
 /*	
